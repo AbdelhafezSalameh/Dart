4ba4de3ab8dbf61861f69a435721b7d8383e4b4e
AbdelhafezSalameh <96789467+AbdelhafezSalameh@users.noreply.github.com>
2023-10-26 10:00:45 +0300

    Update Leap Year Checker.dart

--- Leap Year Checker.dart
+++ Leap Year Checker.dart
@@ -1,1 +1,17 @@
+import 'dart:io';
 
+void main() {
+  stdout.write('Enter a year : ');
+  int year = int.parse(stdin.readLineSync()!);
+
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      print("not leap");
+    } else {
+      print("leap");
+    }
+    ;
+  } else {
+    print("not leap");
+  }
+}
